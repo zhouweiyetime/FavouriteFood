61c5ea1de2168463766929482236716b8cf1a569
weiye zhou <weiye.zhou@griffithuni.edu.au>
2021-04-04 16:25:55 +1000

    list food detail

--- FavouriteFood/DetailView.swift
+++ FavouriteFood/DetailView.swift
@@ -10,7 +10,36 @@ import SwiftUI
 struct DetailView: View {
     @State var food: Food
     var body: some View {
-        Text("Hello, \(food.FoodName)!")
+        VStack(alignment: .leading) {
+            food.FoodImage
+                .resizable()
+                .scaledToFit()
+            List {
+        Text(food.FoodName)
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            .scaledToFit()
+        Text(food.FoodDescription)
+                .font(.body)
+                .fontWeight(.thin)
+                .scaledToFit()
+        Text(food.FoodStory)
+                .padding([.top, .bottom, .trailing])
+        Text("RECIPE")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            .scaledToFit()
+            Text(food.FoodRecipe)
+                
+        Text("INGREDIENT")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            .scaledToFit()
+            Text(food.FoodIngredient)
+            }
+        }
     }
 }
-
