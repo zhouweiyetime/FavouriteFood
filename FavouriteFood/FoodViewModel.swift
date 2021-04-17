c7d1ee5c455fe81e9ad23b1ffd36ff0dd1c9dda2
zhou weiye <109320954@qq.com>
2021-04-18 01:44:04 +1000

    unit test

--- FavouriteFood/FoodViewModel.swift
+++ FavouriteFood/FoodViewModel.swift
@@ -8,11 +8,11 @@
 import Foundation
 import SwiftUI
 
-
+//transfer url to image, download and store image
 class FoodViewModel{
-    
+    //store image and related url
     var imageFoodView = Dictionary<URL, Image>()
-    
+    //convert url image
     func download(_ imageURLString: String) -> Image {
         guard let url = URL(string: imageURLString) else {
             let FoodImage = Image("nonexistent")
--- FavouriteFoodTests/FavouriteFoodTests.swift
+++ FavouriteFoodTests/FavouriteFoodTests.swift
@@ -6,6 +6,8 @@
 //
 
 import XCTest
+import SwiftUI
+
 @testable import FavouriteFood
 
 class FavouriteFoodTests: XCTestCase {
@@ -72,6 +74,40 @@ class FavouriteFoodTests: XCTestCase {
         XCTAssertEqual(food.FoodIngredient, FoodIngredient)
     }
 
+    func testAddElement(){
+    //test add element function
+        var food = [Food]()
+        let foods = Food(FoodImage: "", FoodName: "<New>", FoodDescription: "Enter Description", FoodStory: "Enter Story", FoodRecipe: "Enter Recipe", FoodIngredient: " ")
+        food.append(foods)
+        food.append( Food(FoodImage: cakeImage, FoodName: "cake", FoodDescription: cakeDescription, FoodStory: cakeStory, FoodRecipe: cakeRecipe, FoodIngredient: cakeIngredient))
+        food.append(Food(FoodImage: chickenNuggetsImage, FoodName: "chickenNuggets", FoodDescription: chickenNuggetsDescription, FoodStory: chickenNuggetsStory, FoodRecipe: chickenNuggetsRecipe, FoodIngredient: chickenNuggetsIngredient))
+        XCTAssert(food.count == 3)
+    }
+    
+    func testRemoveElement() {
+    // test remove element function
+        var food : [Food] = [Food(FoodImage: chipsImage, FoodName: "chips", FoodDescription: chipsDescription, FoodStory: chipsStory, FoodRecipe: chipsRecipe, FoodIngredient: chipsIngredient)]
+        food.append( Food(FoodImage: cakeImage, FoodName: "cake", FoodDescription: cakeDescription, FoodStory: cakeStory, FoodRecipe: cakeRecipe, FoodIngredient: cakeIngredient))
+        food.remove(at: 0)
+        XCTAssert(food.count == 1)
+    }
+    
+    func testFoodViewModel_valid(){
+    // test FoodviewModel with valid URL
+    let foodviewmodel = FoodViewModel()
+    let url = "https://i.pinimg.com/originals/b7/79/cc/b779cc239e0c3d07aa948380f8a020bc.jpg"
+    let FoodImage = foodviewmodel.download(url)
+    XCTAssertNotNil(FoodImage)
+    }
+    
+    
+    func testFoodViewModel_invalid() {
+    //test FoodViewModel with invalid URL
+        let foodviewmodel = FoodViewModel()
+        let url = ""
+        let FoodImage = foodviewmodel.download(url)
+        XCTAssertEqual(FoodImage, Image("nonexistent"))
+        }
   
 
     func testPerformanceExample() throws {
